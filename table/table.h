c298aa98f5738b50449bbf367313aaadfc503e76
hanggi <hanggicrown@gmail.com>
2016-10-10 16:38:58 +0900

    upload by gitac

--- table/table.h
+++ table/table.h
@@ -12,11 +12,33 @@ struct Table {
 };
 typedef struct Table TT;
 
-
+/*
+ * @paramaters
+ *      char *field[] = {"App name", "id", "mode", "watching"};
+ *   
+ *      int tmpLen = sizeof(field)/sizeof(char *);
+ * @return
+ *      Table &tt;
+ */
 TT *tableInit(int len, char **field);
 
+/*
+ * @paramaters
+ *      char *field2[] = {"koa.js", "1", "user", "yes"};
+ *   
+ *      int tmpLen = sizeof(field)/sizeof(char *);
+ * @return
+ *      Table &tt;
+ */
 void tablePush(TT *tt, int len, char **con);
 
+/*
+ * @return
+ *      int tableLength;
+ */
 int tableLength(TT *tt);
 
+/*
+ * @return the pretty table!
+ */
 void printTable(TT *tt);
